ac6286f75e569b13754885ff47a0504b6b4c8196
TojiHayha <fernando.aq502@gmail.com>
2024-11-11 22:24:38 -0300

    First Commit Mongodb Create Project

---
+++ src/main/java/com/kamishdb/mongodbk/MongodbkApplication.java
@@ -0,0 +1,13 @@
+package com.kamishdb.mongodbk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kamishdb/mongodbk/MongodbkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamishdb.mongodbk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
